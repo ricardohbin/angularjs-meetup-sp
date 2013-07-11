546ab52b51a597b8ebfbefd1d1aff8c1fd671349
Ricardo Bin <ricardohbin@gmail.com>
2013-07-11 00:28:45 -0300

    post-meetup commit

--- js/app.js
+++ js/app.js
@@ -13,21 +13,49 @@ app.factory("SomeService", function () {
     }
 });
 
-app.controller("Main", ["$scope", "$location", "SomeService", function ($scope, $location, SomeService) {
+app.controller("Main", ["$scope", "$location", "$timeout", "$http", "SomeService", function ($scope, $location, $timeout, $http, SomeService) {
+
     $scope.status = {
-        waiting: true,
-        running: false
+        isWaiting: true,
+        iRunning: false,
+        isSaved: false
     }
+    
+    $scope.data = {};
 
     $scope.changePage = function (url) {
         $location.path(url);
     }
 
     $scope.setName = function (text) {
-        $scope.text = SomeService.toUpper(text);
+        $scope.name = SomeService.toUpper(text);
+    }
+
+    $scope.send = function () {
+        $scope.status.isWaiting = false;
+        $scope.status.isRunning = true;
+
+        $http.post("/some/url", { name: $scope.name }).
+            success(function (data) {
+                $scope.status.isSaved = true;
+                $scope.data = data;
+            }).
+            error(function (data) {
+                $scope.status.isWaiting = true;
+                $scope.status.isRunning = false;
+                $scope.clean(data);
+            });
+    }
+
+    $scope.jquery = function () {
+        $.ajax({});
+    };
+
+    $scope.clean = function () {
+        $timeout(function () {
+            $scope.name = "";
+        }, 1000);
     }
 }]);
-
-
 
 }(window.angular));
--- test/unit/appSpec.js
+++ test/unit/appSpec.js
@@ -1,43 +1,64 @@
 describe("Testing my app with angular and jasmine", function () {
-    var scope = {}, location = '';
-
-    beforeEach(function () {
-        module("MyApp");
-        inject(function (_$rootScope_, _$controller_, _$injector_) {
-            scope = _$rootScope_.$new();
-            location = _$injector_.get("$location");
-            _$controller_("Main", { $scope: scope }); 
-        });
-    });
+    var $scope = {}, location = '';
 
     describe("Main controller", function () {
-
-        it("checking initial status", function () {
-            expect(scope.status.waiting).toBeTruthy();
-            expect(scope.status.running).toBeFalsy();
+        beforeEach(function () { //injecting every time
+            module("MyApp");
+            inject(function (_$controller_, _$rootScope_, _$location_) {
+                $scope = _$rootScope_.$new();
+                _$controller_("Main", { $scope: $scope }); 
+            });
         });
 
-        it("checking dependency injection", function () {
-            spyOn(location, 'path');
-            scope.changePage("http://go.com");
-            expect(location.path).toHaveBeenCalledWith("http://go.com");
+        it("should checks initial status", function () {
+            expect($scope.status.isWaiting).toBeTruthy();
+            expect($scope.status.isRunning).toBeFalsy();
         });
-        
-        it("using inject function on the fly", function () {
+
+        it("should checks using inject", function () { //other ways to inject
             inject(function (_$location_) {
                 spyOn(_$location_, 'path');
-                scope.changePage("http://go.com");
+                $scope.changePage("http://go.com");
                 expect(_$location_.path).toHaveBeenCalledWith("http://go.com");
             });
 
             inject(function (_VERSION_) {
                 expect(_VERSION_).toBe("0.0.1");
             });
-
-            inject(function (_SomeService_) { //possible, but avoid it! Mock it :D
-                expect(_SomeService_.toUpper("aaa")).toBe("AAA");
-            });
         });
 
+        it("should checks using other way to injection", inject(function (_SomeService_) {
+            spyOn(_SomeService_, 'toUpper').andReturn(10);
+            $scope.setName("ok");
+            expect($scope.name).toBe(10); //hhmmmm.... bad...
+        }));
+        
+
+        it("should checks timeout", inject(function (_$timeout_) {
+            $scope.name = 'aaaaaa';
+            $scope.clean();
+            _$timeout_.flush();
+            expect($scope.name).toBe('')
+        }));
+
+        describe("ajax services", function () {
+            var httpExpect;
+
+            it("should request ajax and set isSaved state", inject(function (_$httpBackend_) {
+                _$httpBackend_.expect("POST", "/some/url").respond(200, { info: "Saved!" });
+                $scope.send();
+                _$httpBackend_.flush();
+                expect($scope.data).toEqual({info: "Saved!"}); //fail?
+            }));
+
+            it("should ajax fails", inject(function (_$httpBackend_) {
+                _$httpBackend_.expect("POST", "/some/url").respond(500, 'blablabla');
+                spyOn($scope, 'clean');
+                $scope.send();
+                _$httpBackend_.flush();
+                expect($scope.status.isWaiting).toBeTruthy();
+                expect($scope.clean).toHaveBeenCalledWith('blablabla');
+}));
+        });
     });
 });
